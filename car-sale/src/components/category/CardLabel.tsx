bedfac32b3539bb68c8dbd3dce0712da7ea44d2c
Yurii <stepanuk.ura2001@gmail.com>
2024-04-25 18:30:02 +0300

    fix

--- car-sale/src/components/category/CardLabel.tsx
+++ car-sale/src/components/category/CardLabel.tsx
@@ -1,5 +1,5 @@
 import { Typography } from "@mui/material"
 
 export const CardLabel = ({label}: {label: string}) => {
-    return <Typography variant="body2" sx={{color: 'orange', bgcolor: '#F8F8F8', borderRadius: 1, padding: '3px', fontSize: '12px'}}>{label}</Typography>
+    return <Typography variant="body2" sx={{color: 'orange', bgcolor: '#dddfff', borderRadius: 1, padding: '3px', fontSize: '12px'}}>{label}</Typography>
 }
