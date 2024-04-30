91e37c7495d2e5ca75c2f67d774f77dbd6015d38
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2024-04-30 20:06:01 +0200

    create header

--- car-sale/src/App.tsx
+++ car-sale/src/App.tsx
@@ -1,14 +1,15 @@
 
 import "./App.css";
 import NotFound from "./pages/NotFound/NotFound";
-import {Home} from './pages/Home1';
+import { Home } from './pages/Home1';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { HOME, NOT_FOUND } from './constants/pathName';
 import './App.css';
-
+import Header from "./components/Header/Header";
 const App = () => {
    return (
       <Router>
+         <Header />
          <Routes>
             <Route path={HOME} element={<Home />} />
             <Route path={NOT_FOUND} element={<NotFound />} />
