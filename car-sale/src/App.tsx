d543031ec8c6fb0b3d72120227a6dc490c37eadc
Yurii <stepanuk.ura2001@gmail.com>
2024-04-30 20:47:40 +0300

    changed path name

--- car-sale/src/App.tsx
+++ car-sale/src/App.tsx
@@ -1,20 +1,22 @@
-
-import "./App.css";
-import NotFound from "./pages/NotFound/NotFound";
-import {Home} from './pages/Home1';
+import './App.css';
+import NotFound from './pages/NotFound/NotFound';
+import Home from './components/Home/Home'
+import Categories from './pages/Categories/Categories';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { HOME, NOT_FOUND } from './constants/pathName';
+import { HOME, NOT_FOUND, CATEGORIES } from './constants/pathName';
 import './App.css';
 
 const App = () => {
-   return (
-      <Router>
-         <Routes>
-            <Route path={HOME} element={<Home />} />
-            <Route path={NOT_FOUND} element={<NotFound />} />
-         </Routes>
-      </Router>
-   );
+  return (
+    <Router>
+      <Routes>
+        <Route path={HOME} element={<Home />} />
+
+        <Route path={CATEGORIES} element={<Categories />}></Route>
+        <Route path={NOT_FOUND} element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
